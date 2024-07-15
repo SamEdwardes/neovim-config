da91160abafeb98cf090113d5b3b809afa27a6b4
SamEdwardes <edwardes.s@gmail.com>
2024-07-15 12:31:40 -0700

    Updated to use Lazyvim and complete refresh for 2024 based on video from josean

---
+++ lua/sam/plugins/init.lua
@@ -0,0 +1,5 @@
+return {
+  "nvim-lua/plenary.nvim", -- lua function that many plugins use
+  "christoomey/vim-tmux-navigator", -- tmux & split window navigation
+  "nvim-telescope/telescope-fzf-native.nvim" -- include for telescope to work correctly
+}
---
+++ test.py
@@ -0,0 +1,18 @@
+print("hello world")
+
+x = [1, 2, 3]
+
+for i in x:
+    print(i)
+
+y = (1, 2, 3)
+
+name_of_user = "sam"
+
+
+z = "2"
+
+
+
+
+
